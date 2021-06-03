d39dabd7fba5eb90c5b94c3b772cdf2b6fc81d67
Simon51584 <simon51584@gmail.com>
2021-06-03 16:50:04 -0500

    added more to exercise

---
+++ people_counter_app/index.js
@@ -0,0 +1,19 @@
+// document.getElementById("count-el").innerText = 5
+
+// cmd+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+
+
+// console.log(myAge)
+
+// 1. Create two variables, myAge and humanDogRatio
+let myAge = 37
+let humanDogRatio = 7
+// 2. Multiply the two together and store the result in myDogAge 
+let myDogAge = myAge * humanDogRatio
+// 3. Log myDogAge to the console
+console.log(myDogAge)
